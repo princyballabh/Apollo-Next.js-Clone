60aaaa1c8e3d647c718d70277f10c38d70fd4691
Princy Ballabh <princyballabh13@gmail.com>
2025-05-04 05:07:16 +0530

    updated db.ts

--- src/utils/db.ts
+++ src/utils/db.ts
@@ -1,5 +1,9 @@
 import { MongoClient } from 'mongodb';
 
+declare global {
+    var _mongoClientPromise: Promise<MongoClient> | undefined;
+}
+
 const uri = process.env.MONGODB_URI || 'MONGODB_URI';
 const options = {};
 
@@ -10,7 +14,7 @@ if (!globalThis._mongoClientPromise) {
     client = new MongoClient(uri, options);
     globalThis._mongoClientPromise = client.connect();
 }
-clientPromise = globalThis._mongoClientPromise as Promise<MongoClient>;
+clientPromise = globalThis._mongoClientPromise;
 
 export async function connectToDatabase() {
     const client = await clientPromise;
